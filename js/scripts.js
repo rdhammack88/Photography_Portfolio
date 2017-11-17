e44a0d5aced2f672fc1812f0ce3449a5947d387d
Dustin Hammack <rdhammack88@hotmail.com>
2017-11-17 01:18:53 -0600

    Add files via upload
    FINISHED - A photography portfolio written in HTML, SCSS, JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,10 @@
 var jumboImage = document.getElementById('jumbotronImage').firstElementChild;
 var imageElements = document.getElementsByClassName('item');
 imageElements = Array.from(imageElements);
+var previousImage = document.getElementById('prev');
+var nextImage = document.getElementById('next');
+var imageIndex = 0;
 var imageList = [];
-
 var imgholder;
 
 jumboImage.src = imageElements[0].firstElementChild.firstElementChild.src;
@@ -25,30 +27,14 @@ for(let i = 10; i < imageElements.length; i++) {
 
 // FOR EACH OF THE div.item ELEMENTS, ADD A CLICK FUNCTION
 imageElements.forEach(function(el) {
-	//el.addEventListener();
 	el.firstElementChild.firstElementChild.addEventListener('click', function() {
-		imgholder = this; //.parentElement.parentElement;
-		
-		
-		console.log(imgholder);
-		console.log(imgholder.parentElement.parentElement);
-		
+		imgholder = this;
+
 		imageIndex = imageList.indexOf(imgholder.parentElement.parentElement);
-		console.log(imageIndex);
-		
-		
-		
+
 		jumboImage.src = this.src;
 	})
 })
-
-
-// TESTS FOR THE CONSOLE
-console.log(jumboImage);
-console.log(imageList);
-console.log(imageElements);
-console.log(imgholder);
-
 
 // BUTTON ELEMENTS TO LOAD AND HIDE DIV.ITEM ELEMENTS
 var loadMoreImages = document.getElementById('load');
@@ -59,7 +45,6 @@ loadMoreImages.addEventListener('click', function() {
 	document.getElementById('hide').classList.remove('hidden');
 	
 	var activeImagesLength = imageList.length;
-	console.log(activeImagesLength);
 	
 	for(var i = activeImagesLength;
 		i < activeImagesLength + 10; 
@@ -75,31 +60,23 @@ loadMoreImages.addEventListener('click', function() {
 	}
 	
 	hideShowLoad();
-	
-		console.log(imageList.length);
 })
-
 
 // HIDE IMAGES FROM THE SCREEN BUTTON
 hideSomeImages.addEventListener('click', function() {
-	
 	var activeImagesLength = imageList.length;
-	console.log(activeImagesLength);
 	
 	if(activeImagesLength % 10 === 0) {
 		var i = activeImagesLength - 1;
 		var j = 10;
-		console.log('line71');
 	} else {
 		var i = activeImagesLength - 1;
 		var j = activeImagesLength % 10;
-		console.log('line75');
 	}
 	
 	for( i;
 		 i >= activeImagesLength - j; 
 		 i -= 1 ) {
-		console.log('line81');
 		imageElements[i].classList.add('hidden');
 		imageElements[i].classList.remove('active');
 		imageList.pop();
@@ -107,85 +84,54 @@ hideSomeImages.addEventListener('click', function() {
 	
 	if( imageList.length > 10 ) {
 		hideSomeImages.classList.remove('hidden');
-		console.log('line89');
 	} else {
 		hideSomeImages.classList.add('hidden');
-		console.log('line92');
 	}
 	
 	hideShowLoad();
-	
-	console.log(imageList.length);
 })
 
 
 function hideShowLoad() {
 	if( imageList.length === imageElements.length ) {
 		loadMoreImages.classList.add('hidden');
-		console.log('line89');
 	} else {
 		loadMoreImages.classList.remove('hidden');
-		console.log('line92');
 	}
 }
-
-
-var previousImage = document.getElementById('prev');
-var nextImage = document.getElementById('next');
-var imageIndex = 0;
 
 previousImage.addEventListener('click', function() {
 	imageIndex--;
 	
 	if( imageIndex < 0 ) {
-		imageIndex = imageList.length - 1; //imageElements
+		imageIndex = imageList.length - 1;
 	}
 	
 	if(imgholder) {
-		//jumboImage.src = imgholder.previousElementSibling.firstElementChild.firstElementChild.src;
-		
 		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src;
-		
-//		var imgParent = imgholder.parentNode.parentElement;
-//		console.log(imgParent);
-//		imageIndex = imageList.indexOf(imgholder.parent.parent);
-//		console.log(imageIndex);
 		
 		imgholder = '';
 	} else {
-		//imageIndex--;
-		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src; //imageElements
+		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src;
 	}
 })
 
 nextImage.addEventListener('click', function() {
 	imageIndex++;
-	console.log(imageIndex);
 	
-	if( imageIndex >= imageList.length ) { //imageElements
+	if( imageIndex >= imageList.length ) {
 		imageIndex = 0;
 	}
 	
 	if(imgholder) {
-		//jumboImage.src = imgholder.nextElementSibling.firstElementChild.firstElementChild.src;
-		
 		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src;
 		
-//		var imgParent = imgholder.parentNode.parentElement;
-//		console.log(imgParent);
-//		imageIndex = imageList.indexOf(imgholder);
-//		console.log(imageIndex);
-			
 		imgholder = '';
 	} else {
 		//imageIndex++;
-		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src; //imageElements
+		jumboImage.src = imageList[imageIndex].firstElementChild.firstElementChild.src;
 	}
 })
-
-
-
-
 
 /*
 when image is clicked and shown in jumbotron, imgholder = this image 
